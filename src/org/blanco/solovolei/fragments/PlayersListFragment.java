5e0dfba1c4392386142b49b07f7b6d3ec4fc54c7
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2011-12-01 08:26:35 -0700

    Started working in the content provider, this should be developed separetely and merged later. The content provider is a very delicated class and should be defined very well

--- src/org/blanco/solovolei/fragments/PlayersListFragment.java
+++ src/org/blanco/solovolei/fragments/PlayersListFragment.java
@@ -1,21 +1,37 @@
 package org.blanco.solovolei.fragments;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
-public class PlayersListFragment extends Fragment {
+public class PlayersListFragment extends ListFragment {
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		
-		return inflater.inflate(org.blanco.solovolei.R.layout.players_list_layout, null);
+	public void onStart() {
+		findPlayersAndPopulateList();
+		super.onStart();
 	}
 
+//	@Override
+//	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//			Bundle savedInstanceState) {
+//		
+//		return inflater.inflate(org.blanco.solovolei.R.layout.players_list_layout, null);
+//	}
+
 	
-		
+	
+	public void findPlayersAndPopulateList(){
+		Cursor c = getActivity().managedQuery(Uri.parse("content://org.blanco.solovolei/players"), 
+				null, null, null, null);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+		while(c.moveToNext()){
+			String nombre = c.getString(c.getColumnIndex("nombre"));
+			adapter.add(nombre);
+		}
+		setListAdapter(adapter);
+	}
+	
 	
 }
---
+++ src/org/blanco/solovolei/providers/SoloVoleiContentProvider.java
@@ -0,0 +1,51 @@
+package org.blanco.solovolei.providers;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+
+public class SoloVoleiContentProvider extends ContentProvider {
+
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri uri, ContentValues values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCreate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection,
+			String[] selectionArgs, String sortOrder) {
+		MatrixCursor cursor = new MatrixCursor(new String[]{"_id", "nombre", "numero"});
+		cursor.addRow(new Object[]{1,"Alex",3});
+		cursor.addRow(new Object[]{1,"Carlos",5});
+		return cursor;
+	}
+
+	@Override
+	public int update(Uri uri, ContentValues values, String selection,
+			String[] selectionArgs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
