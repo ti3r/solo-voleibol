69ef598a949f261d125640bdbcd0d191c86078a2
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2012-02-09 17:03:23 -0700

    Adding missing files to the index, last commit wont work with out this.

---
+++ README.txt
@@ -0,0 +1,11 @@
+Solo-voleibol is an android application that helps you keep track all the 
+events that happen during a volley-ball match in order to be reviewed later
+and build database of information. These statistics are important for every
+volley-ball coach to see the strengths and weakness of the teams he/she 
+trains.
+It is designed to work very good with tables and cell-phones so you can't 
+miss any match of your teams.
+Solo-voleibol is licensed under the MIT license and you can use it for any
+purposes, including commercial, without warranty of any kind. You can find 
+a copy of this license under: 
+http://www.opensource.org/licenses/mit-license.php
---
+++ src/org/blanco/solovolei/misc/ExpandAnimation.java
@@ -0,0 +1,59 @@
+/**
+ * Tec ch mun 2011 for Android, is the android application used to 
+ *  
+ * review all the information that is generated during the event
+ * Tec Ch Mun 2011 of the ITESM campus chihuahua.
+ * You can use this application as an example of all the technologies
+ * used in this app.
+ * Copyright (C) 2011  Alexandro Blanco <ti3r.bubblenet@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Visit http://tec-ch-mun-2011.herokuapps.com
+ */
+package org.blanco.solovolei.misc;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class ExpandAnimation extends Animation {
+        private final View _view;
+        private final int _startHeight;
+        private final int _finishHeight;
+
+        public ExpandAnimation( View view, int startHeight, int finishHeight ) {
+            _view = view;
+            _startHeight = startHeight;
+        _finishHeight = finishHeight;
+        setDuration(220);
+    }
+
+    @Override
+    protected void applyTransformation( float interpolatedTime, Transformation t ) {
+        final int newHeight = (int)((_finishHeight - _startHeight) * interpolatedTime + _startHeight);
+        _view.getLayoutParams().height = newHeight;
+        _view.requestLayout();
+    }
+
+    @Override
+    public void initialize( int width, int height, int parentWidth, int parentHeight ) {
+        super.initialize(width, height, parentWidth, parentHeight);
+    }
+
+    @Override
+    public boolean willChangeBounds( ) {
+        return true;
+    }
+}
+	
