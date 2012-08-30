8411c78564a93773ce402a8f17607456a95f97a0
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2012-08-30 14:06:18 -0500

    Remove un used imports

--- src/org/blanco/solovolei/GameActivity.java
+++ src/org/blanco/solovolei/GameActivity.java
@@ -36,7 +36,6 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
